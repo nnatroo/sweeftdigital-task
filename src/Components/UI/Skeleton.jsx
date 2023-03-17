59ba3b3aad284d56d077d0c3849fa5ce9d507de7
nnatroo <nugonatroshvili@gmail.com>
2023-03-18 01:42:02 +0400

    added infinite scrool

---
+++ src/Components/UI/Skeleton.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Skeleton from '@mui/material/Skeleton';
+import classes from "../Modules/User.module.css";
+
+export default function Variants() {
+  return (
+    <div className={classes['user-card']}>
+        <Skeleton variant="rectangular" width={"100%"} height={"100%"} />
+    </div>
+        
+  );
+}
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,34 +1,69 @@
 import User from "./User";
 import classes from "../Modules/Users.module.css";
 import { useEffect, useState } from "react";
+import Loading from "../UI/Loading";
+import Skeleton from "../UI/Skeleton";
 
 const Users = (props) => {
   const [usersData, setUsersData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showLoading, setShowLoading] = useState(true);
 
   useEffect(() => {
     fetch(
-      "http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/1/100"
+      `http://sweeftdigital-intern.eu-central-1.elasticbeanstalk.com/user/${currentPage}/20`
     )
       .then((response) => response.json())
       .then((users) => {
-        setUsersData(users.list);
+        setUsersData((prevUsersData) => [...prevUsersData, ...users.list]);
+        setShowLoading(false);
       });
-  }, []);
-  
-  console.log(usersData);
+  }, [currentPage]);
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // });
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
+    ) {
+      setShowLoading(true);
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // console.log(usersData);
 
   return (
-    <div className={classes.users}>
-      {usersData.map((user, index) =>
-        <User key={user.id}
-        id={user.id}
-        imageUrl={user.imageUrl}
-        name={user.name}
-        lastName={user.lastName}
-        prefix={user.prefix}
-        title={user.title}
-        />)}
-    </div>
+    <>
+      <div className={classes.users}>
+        {usersData.map((user, index) => (
+          <User
+            key={user.id}
+            id={user.id}
+            imageUrl={user.imageUrl}
+            name={user.name}
+            lastName={user.lastName}
+            prefix={user.prefix}
+            title={user.title}
+          />
+        ))}
+        <Skeleton animation="wave" width={"100%"} height={"100%"}/>
+        <Skeleton animation="wave" width={"100%"} height={"100%"}/>
+        <Skeleton animation="wave" width={"100%"} height={"100%"}/>
+        <Skeleton animation="wave" width={"100%"} height={"100%"}/>
+        <Skeleton animation="wave" width={"100%"} height={"100%"}/>
+        <Skeleton animation="wave" width={"100%"} height={"100%"}/>
+        <Skeleton animation="wave" width={"100%"} height={"100%"}/>
+        <Skeleton animation="wave" width={"100%"} height={"100%"}/>
+      </div>
+
+      {showLoading && <Loading />}
+    </>
   );
 };
 
