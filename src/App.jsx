c4730fd9d309676efdb13ab34059718384acf877
nnatroo <nugonatroshvili@gmail.com>
2023-03-18 00:25:59 +0400

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from "react";
 import "./Components/Modules/App.module.css"
 import Users from "./Components/Users/Users";
 
 function App() {
-  
-
   return (
     <Users />
   );
